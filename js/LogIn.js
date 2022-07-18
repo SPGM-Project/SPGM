7f5fb52e613ffad797eb1a0a4d28e4e136a32b44
Seungjee Lee <moondgod@gmail.com>
2022-07-18 11:10:32 +0900

    loginModify

---
+++ js/LogIn.js
@@ -0,0 +1,14 @@
+const forgot = document.querySelector(".forgot");
+const Inputs = document.querySelector(".Inputs");
+const logBTN = document.querySelector(".LogInBTN");
+function handleAlert(){
+    alert("그걸 까먹냐");
+}
+function handleSubmit(event){
+    event.preventDefault();
+    alert("id나 비밀번호가 일치하지 않습니다");
+}
+
+forgot.addEventListener("click", handleAlert);
+Inputs.addEventListener("submit", handleSubmit);
+logBTN.addEventListener("click", handleSubmit);
