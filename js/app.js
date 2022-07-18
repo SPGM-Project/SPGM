f8802b7804fa88eff1dbc60407b03ccd8baceb22
KIMHUEMANG <piyrw9754@gmail.com>
2022-07-18 21:12:21 +0900

    main_Course2

--- js/app.js
+++ js/app.js
@@ -19,3 +19,13 @@ function handleScroll(){
 
 
 course.addEventListener("mouseover", handleScroll);
+
+// new Swiper('.class .swiper',{
+//     direction : "horizontal",
+//     loop : true,
+//     slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+//     spaceBetween : 10, // 슬라이드 사이 여백
+//     centeredSlides : true, // 1번 슬라이드 가운데 보이기
+//     debugger : true,
+// })
+
